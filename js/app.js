78a0c2c271a4b9fa7a109f38f9c38d28bcb70d48
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-21 09:49:26 +0600

    done

--- js/app.js
+++ js/app.js
@@ -12,6 +12,18 @@ const saveBtn = document.getElementById('saveing')
 const savingAmount = document.getElementById('saving-amount')
 const remainingBalance = document.getElementById('remaining-amount')
 
+// show massage 
+
+const showMassage = (text, status) => {
+    massage.textContent = text
+    massage.classList.add(`bg-${status}`)
+    setTimeout(() => {
+      massage.textContent = ''
+      massage.classList.remove(`bg-${status}`)
+    }, 1000)
+  }
+
+
 // functions
 
 const parseIntoNumber = (value) => {
@@ -53,13 +65,22 @@ if (isNaN(totalBalanceNumber)) {
 
 
 const getPercentage = (partialValue,totalValue) => {
+
+    if(0<= partialValue <100 && 100> partialValue){
+
+        
+        const savings = (totalValue * partialValue)/100
+        savingAmount.innerText=savings 
+       
+       
+       
+        return savings
+
+    }else{
+        showMassage('invalid number','danger')
+    }
    
-const savings = (totalValue * partialValue)/100
- savingAmount.innerText=savings 
 
-
-
- return savings
    
 }
 
@@ -74,10 +95,16 @@ saveBtn.addEventListener('click',()=>{
     const percent = document.getElementById('save')
    const savings = getPercentage(parseIntoNumber(percent),parseIntoNumber(incomeValue));
 
+   
    const remaining = totalBalance - savings
+   if (isNaN(remaining)) {
+    showMassage('please input within 1 to 100','danger')
+}else{
+    remainingBalance.innerText = remaining 
+}
 
 
-  remainingBalance.innerText = remaining
+  
 
    
    
