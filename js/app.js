8c9b8904d7cf642edb4eab3d84ac05e7d2089d66
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-21 09:10:28 +0600

    done

--- js/app.js
+++ js/app.js
@@ -8,39 +8,79 @@ const saveValue = document.getElementById('save');
 const calcBtn = document.getElementById('calculate')
 const exp = document.getElementById('exp')
 const balance = document.getElementById('balance')
+const saveBtn = document.getElementById('saveing')
+const savingAmount = document.getElementById('saving-amount')
+const remainingBalance = document.getElementById('remaining-amount')
 
 // functions
 
-const parseIntoNumber = (value)=>{
-const strValue = value.value
-const numberValue = parseFloat(strValue)
+const parseIntoNumber = (value) => {
+    const strValue = value.value
+    const numberValue = parseFloat(strValue)
 
-if (isNaN(numberValue)) {
-    return 0
-}else{
-    return numberValue
+    if (isNaN(numberValue)) {
+        return 0
+    } else {
+        return numberValue
+    }
+
 }
 
-}
 
 
 
-const calculation = ()=>{
-    
-    const totalExp = parseIntoNumber(foodValue)+ parseIntoNumber(rentValue)+ parseIntoNumber(clothValue)
+
+const calculation = () => {
+
+    const totalExp = parseIntoNumber(foodValue) + parseIntoNumber(rentValue) + parseIntoNumber(clothValue)
     exp.innerText = totalExp;
 
     const income = parseIntoNumber(incomeValue);
-    const totalBalance =  income -totalExp ;;
+    const totalBalance = income - totalExp;;
     balance.innerText = totalBalance;
-
-    return totalBalance
-
+    remainingBalance.innerText = totalBalance
+    const totalBalanceNumber = parseFloat(totalBalance)
+ 
+if (isNaN(totalBalanceNumber)) {
+   alert('its be a number')
+    
+}else{
+    return totalBalanceNumber
+}
 
 }
 
 
+
+const getPercentage = (partialValue,totalValue) => {
+   
+const savings = (totalValue * partialValue)/100
+ savingAmount.innerText=savings 
+
+
+
+ return savings
+   
+}
+
+ 
+
 // write onClick function on Calculate
 
-calcBtn.addEventListener('click',calculation)
+calcBtn.addEventListener('click', calculation)
+saveBtn.addEventListener('click',()=>{
+    const totalBalance = calculation();
+ 
+    const percent = document.getElementById('save')
+   const savings = getPercentage(parseIntoNumber(percent),parseIntoNumber(incomeValue));
+
+   const remaining = totalBalance - savings
+
+
+  remainingBalance.innerText = remaining
+
+   
+   
+})
+
 
